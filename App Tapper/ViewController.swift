95ce9860a6093d759e45e90a9cf724fe70d76d6c
SEBASTIEN MONTIBELLER <sebastienmontibeller@SEBASTIsMacBook.gateway>
2016-02-03 22:33:44 +1100

    half way through

--- App Tapper/ViewController.swift
+++ App Tapper/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+    logoImg.hidden = true
+    howManyTapsTxt.hidden = true
+    playBtn.hidden = true
+        
+    tapBtn.hidden = false
+        tapsLbl.hidden = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
